79af59430990028d47e72220a816789096e347f0
Akos Horvath <akos.hrv@gmail.com>
2020-05-13 14:38:01 +0200

    Consume local product service endpoint

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    products : []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/api/v1/products')
+    .then(result => result.json())
+    .then((data) => {
+      this.setState({products: data})
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.products.map((product) => (
+          <div>
+            Name: {product.name}
+          </div>
+        ))}
       </div>
     );
   }
